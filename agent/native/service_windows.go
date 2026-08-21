e79f2ab4545a4067ac0996eabec4a02157a96f3c
PleaseMahobo <security@cyberbastion.co.za>
2026-08-22 00:42:45 +0200

    fix: align Windows service wrapper with agent runtime

--- agent/native/service_windows.go
+++ agent/native/service_windows.go
@@ -2,47 +2,28 @@
 
 package main
 
-import (
-	"context"
-	"fmt"
-
-	"golang.org/x/sys/windows/svc"
-)
+import "golang.org/x/sys/windows/svc"
 
 type bhudiWindowsService struct {
 	server string
-	cancel context.CancelFunc
-	done   chan struct{}
 }
 
 func (s *bhudiWindowsService) Execute(_ []string, req <-chan svc.ChangeRequest, status chan<- svc.Status) (bool, uint32) {
 	const accepted = svc.AcceptStop | svc.AcceptShutdown
 	status <- svc.Status{State: svc.StartPending}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	s.cancel = cancel
-	s.done = make(chan struct{})
-	go func() {
-		defer close(s.done)
-		runAgent(ctx, runConfig{Server: s.server, Interval: 10})
-	}()
-
+	go runAgent(runConfig{Server: s.server, Interval: 10})
 	status <- svc.Status{State: svc.Running, Accepts: accepted}
+
 	for c := range req {
 		switch c.Cmd {
 		case svc.Interrogate:
 			status <- svc.Status{State: svc.Running, Accepts: accepted}
 		case svc.Stop, svc.Shutdown:
 			status <- svc.Status{State: svc.StopPending}
-			cancel()
-			<-s.done
 			return false, 0
-		default:
-			return false, uint32(svc.StopReasonOther)
 		}
 	}
-	cancel()
-	<-s.done
 	return false, 0
 }
 
@@ -55,14 +36,5 @@ func runWindowsService(server string) error {
 
 func isWindowsServiceProcess() bool {
 	ok, err := svc.IsWindowsService()
-	if err != nil {
-		return false
-	}
-	return ok
-}
-
-func serviceFatal(err error) {
-	if err != nil {
-		fatal(fmt.Errorf("windows service: %w", err))
-	}
+	return err == nil && ok
 }
