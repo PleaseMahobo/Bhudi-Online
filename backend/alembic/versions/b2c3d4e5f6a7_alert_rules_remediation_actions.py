e0723c3b4e53bfd61130f60e8b1c3d4ca033157f
PleaseMahobo <security@cyberbastion.co.za>
2026-08-31 14:04:44 +0200

    fix(alembic): assign unique revision ID to remediation migration

--- backend/alembic/versions/b2c3d4e5f6a7_alert_rules_remediation_actions.py
+++ backend/alembic/versions/b2c3d4e5f6a7_alert_rules_remediation_actions.py
@@ -1,13 +1,13 @@
 """add remediation_actions to alert_rules
 
-Revision ID: b2c3d4e5f6a7
+Revision ID: c4d5e6f7a8b9
 Revises: a1b2c3d4e5f6
 Create Date: 2026-08-26
 """
 from alembic import op
 import sqlalchemy as sa
 
-revision = "b2c3d4e5f6a7"
+revision = "c4d5e6f7a8b9"
 down_revision = "a1b2c3d4e5f6"
 branch_labels = None
 depends_on = None
