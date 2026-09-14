628157de31f2334f2e996343ff6b2a6d37b9836e
PleaseMahobo <security@cyberbastion.co.za>
2026-09-14 12:06:37 +0200

    Fix desktop worker Windows imports

--- agent/native/desktop_worker_windows.go
+++ agent/native/desktop_worker_windows.go
@@ -8,40 +8,30 @@ import (
     "os"
     "path/filepath"
     "strings"
+    "unsafe"
 
     "golang.org/x/sys/windows"
 )
 
 func launchDesktopWorker(serverURL, sessionID, sessionMode, displayProtocol string, monitorIndex int) error {
     sid, err := activeInteractiveSessionID()
-    if err != nil {
-        return err
-    }
+    if err != nil { return err }
     var token windows.Token
-    if err := windows.WTSQueryUserToken(sid, &token); err != nil {
-        return fmt.Errorf("query user token for session %d: %w", sid, err)
-    }
+    if err := windows.WTSQueryUserToken(sid, &token); err != nil { return fmt.Errorf("query user token for session %d: %w", sid, err) }
     defer token.Close()
-
     exe, err := os.Executable()
-    if err != nil {
-        return fmt.Errorf("resolve agent executable: %w", err)
-    }
+    if err != nil { return fmt.Errorf("resolve agent executable: %w", err) }
     exe, err = filepath.Abs(exe)
-    if err != nil {
-        return fmt.Errorf("resolve agent executable path: %w", err)
-    }
-
+    if err != nil { return fmt.Errorf("resolve agent executable path: %w", err) }
     commandLine := fmt.Sprintf("\"%s\" desktop-worker -server \"%s\" -session \"%s\" -mode \"%s\" -protocol \"%s\" -monitor %d", exe, serverURL, sessionID, sessionMode, displayProtocol, monitorIndex)
     desktop, _ := windows.UTF16PtrFromString("winsta0\\default")
     cmdline, _ := windows.UTF16PtrFromString(commandLine)
-
     var si windows.StartupInfo
     si.Cb = uint32(unsafe.Sizeof(si))
     si.Desktop = desktop
     var pi windows.ProcessInformation
     flags := uint32(windows.CREATE_UNICODE_ENVIRONMENT | windows.CREATE_NEW_PROCESS_GROUP)
-    if err := windows.CreateProcessAsUser(token, exe, cmdline, nil, nil, false, flags, nil, nil, &si, &pi); err != nil {
+    if err := windows.CreateProcessAsUser(token, nil, cmdline, nil, nil, false, flags, nil, nil, &si, &pi); err != nil {
         return fmt.Errorf("CreateProcessAsUser desktop worker session %d: %w", sid, err)
     }
     windows.CloseHandle(pi.Thread)
